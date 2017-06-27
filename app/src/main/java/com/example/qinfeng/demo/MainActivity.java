a805bc982c03b994f3f106784b206b2d13b4c0e4
Qin.Feng <396558603@qq.com>
2017-06-27 16:29:19 +0800

    注释：二次修改

--- app/src/main/java/com/example/qinfeng/demo/MainActivity.java
+++ app/src/main/java/com/example/qinfeng/demo/MainActivity.java
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String a="0;";
+        String a="0;1";
     }
 }
